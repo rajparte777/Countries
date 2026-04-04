250e0cb7a6f6c089114771dd0af9f8ceca066454
project demo <parteraj2005@gmail.com>
2026-04-04 22:51:14 +0530

    push all code in git

---
+++ script.js
@@ -0,0 +1,40 @@
+ const countryContainer = document.querySelector('.country-container')
+
+
+fetch('https://restcountries.com/v3.1/all?fields=flags,name,capital,currencies,population,region,capital')
+.then((res)=>res.json())
+.then((data)=>{
+   data.forEach((country)=>{
+        const countryCard = document.createElement('a');
+ countryCard.classList.add('card');
+
+ const imageContainer = document.createElement('div');
+ imageContainer.classList.add('image')
+ const image = document.createElement('img');
+ image.src =`${country.flags.svg}`;
+ imageContainer.append(image);
+countryCard.append(imageContainer);
+
+
+const title = document.createElement('div');
+title.classList.add('data-country');
+const header = document.createElement('h2');
+
+header.innerText =`${country.name.common}` ;
+title.append(header);
+const p1 = document.createElement('p');
+p1.innerHTML =` <strong>Population :</strong>${country.population.toLocaleString('en-In')}`;
+const p2 = document.createElement('p');
+p2.innerHTML = `<strong>Region :</strong> ${country.region}`;
+const p3 = document.createElement('p');
+p3.innerHTML = `<strong>Capital :</strong> ${country.capital}`;
+const p4 = document.createElement('p');
+
+title.append(p1, p2, p3, p4);
+countryCard.append(title);
+
+countryContainer.append(countryCard);
+    })
+})
+
+console.log(countryContainer);
