c5fbcd07cdf23ae3515a8bfe6d87f2fb077b5248
project demo <parteraj2005@gmail.com>
2026-04-08 13:59:55 +0530

    update js file of country.js

--- country.js
+++ country.js
@@ -1,7 +1,45 @@
 const countryNameUrl = new URLSearchParams(window.location.search).get('name');
+const sizeBox =document.querySelector('.size-box');
+
 
 fetch(`https://restcountries.com/v3.1/name/${countryNameUrl}?fullText=true`)
 .then((res) => res.json())
 .then((data) => {
     console.log(data[0]);
+   
+
+   sizeBox.innerHTML =`
+    <div class="back-button">
+         <button ><i class="fa-solid fa-arrow-left"></i>&nbsp;Back</button>
+      </div>
+       <section>
+       <div class="counnrty-container">
+         <div class="imgCountainer">
+               <img src="https://flagcdn.com/ck.svg", alt="">
+       </div>
+       <div class="country-detail-1">
+        <h2>`${country.name.common}`</h2>
+        <p><b>Native Name :</b></p>
+        <p><b>Population :</b></p>
+        <p><b>Region :</b></p>
+        <p><b>Sub Region :</b></p>
+        <p><b>Capital :</b></p>
+       </div>
+     <div class="country-detail-2">
+         <p><b>Top Level Domain :</b></p>
+        <p><b>Currencies :</b></p>
+        <p><b>Language :</b></p>
+     </div>
+    
+       </div>
+       <div class="country-detail-3">
+          <p><b>Border Countries :</b></p>
+          <span><button>France</button>
+        <button>German</button>
+    <button>netharland</button></span>
+       </div>
+       
+       </section>`
+
+        
 })
