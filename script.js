e959b5c64da31c423c09ca5e157ce2055c7935cd
project demo <parteraj2005@gmail.com>
2026-06-09 22:26:37 +0530

    search function is working proprly

--- script.js
+++ script.js
@@ -1,47 +1,17 @@
  const countryContainer = document.querySelector('.country-container')
 const filterByRegion = document.querySelector('.filter-by-region')
+const search = document.querySelector('.search input')
+
+let allCountriesData
 
 fetch('https://restcountries.com/v3.1/independent?status=true')
 .then((res)=>res.json())
 .then((data)=>{
-   data.forEach((country)=>{
-    // console.log([country.borders]);
-  
-
-
-    
-        const countryCard = document.createElement('a');
-        countryCard.href =`/country.html?name=${country.name.common}`;
- countryCard.classList.add('card');
-
- const imageContainer = document.createElement('div');
- imageContainer.classList.add('image')
- const image = document.createElement('img');
- image.src =`${country.flags.svg}`;
- imageContainer.append(image);
-countryCard.append(imageContainer);
-
-
-const title = document.createElement('div');
-title.classList.add('data-country');
-const header = document.createElement('h2');
-
-header.innerText =`${country.name.common}` ;
-title.append(header);
-const p1 = document.createElement('p');
-p1.innerHTML =` <strong>Population :</strong>${country.population.toLocaleString('en-In')}`;
-const p2 = document.createElement('p');
-p2.innerHTML = `<strong>Region :</strong> ${country.region}`;
-const p3 = document.createElement('p');
-p3.innerHTML = `<strong>Capital :</strong> ${country.capital}`;
-const p4 = document.createElement('p');
-
-title.append(p1, p2, p3, p4);
-countryCard.append(title);
-
-countryContainer.append(countryCard);
-    })
+    renderCountriesData(data)
+    allCountriesData =data
+    console.log(allCountriesData);
 })
+
 
 
 filterByRegion.addEventListener('change',(e)=>{
@@ -49,13 +19,15 @@ filterByRegion.addEventListener('change',(e)=>{
 
 fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
 .then((res)=>res.json())
-.then((data)=>{
-    countryContainer.innerText =''
+.then((renderCountriesData))
+
+
+})
+
+function renderCountriesData(data){
+     countryContainer.innerText =''
    data.forEach((country)=>{
-    // console.log([country.borders]);
-    console.log(country);
- 
-        const countryCard = document.createElement('a');
+  const countryCard = document.createElement('a');
         countryCard.href =`/country.html?name=${country.name.common}`;
  countryCard.classList.add('card');
 
@@ -85,11 +57,27 @@ title.append(p1, p2, p3, p4);
 countryCard.append(title);
 
 countryContainer.append(countryCard);
-    })
+}
+   )}
+
+
+
+search.addEventListener('input', (e)=>{
+//   console.log(e.target.value);
+const filterCountries =allCountriesData.filter((country) =>country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+console.log(filterCountries);
+renderCountriesData(filterCountries)
 })
 
 
-})
 
 
-console.log(countryContainer);
+
+
+
+
+
+
+   
+
+// console.log(countryContainer);
